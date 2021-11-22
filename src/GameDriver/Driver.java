71a3ec014bd4774c9f503dfebc913f9c333e4869
Vygintas2 <vygintas.marciulaitis@stud.vgtu.lt>
2021-11-22 19:43:01 +0200

    Initial commit

---
+++ src/GameDriver/Driver.java
@@ -0,0 +1,12 @@
+package GameDriver;
+
+import UserInterface.Menu;
+
+public class Driver
+{
+	public static void main(String[] Args)
+	{
+		// Start the game with a menu.
+		new Menu("Minesweeper");
+	}
+}
